a4b18f676b1694401c75066c226bec6ad8d5abee
Turtel216 <papadimitrios216@gmail.com>
2024-06-19 00:39:11 +0300

    Fixed rle issue

--- rle.go
+++ rle.go
@@ -7,11 +7,12 @@ Lossless compression using the Run-length encoding algorithm.
 Best suited for compressing strings with a lot of repeating
 characters
 */
-func RLE_compress(str string) string {
+func RLE_compress(str string) string { //TODO fixe issue when duplicates are on the end of the list
 
-	var prev_char rune // Store the previous character
-	var first_duplicate int
-	var duplicate_number int = 0
+	var prev_char rune           // Stores the previous character
+	var first_duplicate int      // tracks the index of the first duplicate
+	var duplicate_number int = 0 // counts the number of duplicates
+	var offset int = 0           // adjusts index to changes made
 
 	// Iterate over the string
 	for i, _char := range str {
@@ -22,26 +23,36 @@ func RLE_compress(str string) string {
 
 				// Check if this is the first duplicate
 				if duplicate_number == 0 {
-					first_duplicate = i
+					first_duplicate = i - 1
 					duplicate_number++
+
+					// set previous character
+					prev_char = _char
 					continue
 				}
 
+				// set previous character
+				prev_char = _char
 				duplicate_number++
+				continue
 			}
 
 			// Compress duplicates
 			if prev_char != _char && duplicate_number != 0 {
 				// Added previous duplicate
 				duplicate_number++
-				// Replace first duplicate and remove the rest of the duplicates
-				str = str[:first_duplicate-1] + fmt.Sprint(duplicate_number) + str[i-1:]
-				fmt.Println("String after deletetion: " + str)
 
+				// Replace first duplicate and remove the rest of the duplicates
+				str = str[:first_duplicate+offset] + fmt.Sprint(duplicate_number) + str[i-1+offset:]
+
+				// Calculate offset
+				offset = offset - duplicate_number + 2
 				// Reset duplicate counter
 				duplicate_number = 0
+
 				// set previous character
 				prev_char = _char
+				continue
 			}
 		}
 
--- rle_test.go
+++ rle_test.go
@@ -3,8 +3,8 @@ package gocompress
 import "testing"
 
 func TestRlE_compression(t *testing.T) {
-	test_input := "aaabcdd"
-	test_expected_output := "3abc2d"
+	test_input := "aaabcddaf"
+	test_expected_output := "3abc2daf"
 	test_output := RLE_compress(test_input)
 	if test_output != test_expected_output {
 		t.Error("Failted rle compression on string: " + test_input + ". Output was: " + test_output)
