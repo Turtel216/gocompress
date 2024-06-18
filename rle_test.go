1f4204c8058311cec54ca0f6a4cc23d3816bf557
Turtel216 <papadimitrios216@gmail.com>
2024-06-19 00:52:14 +0300

    Updated rle tests

--- rle_test.go
+++ rle_test.go
@@ -9,4 +9,17 @@ func TestRlE_compression(t *testing.T) {
 	if test_output != test_expected_output {
 		t.Error("Failted rle compression on string: " + test_input + ". Output was: " + test_output)
 	}
+
+	test_input = "bcssssdda"
+	test_expected_output = "bc4s2da"
+	test_output = RLE_compress(test_input)
+	if test_output != test_expected_output {
+		t.Error("Failted rle compression on string: " + test_input + ". Output was: " + test_output)
+	}
+
+	test_input = "bcsda"
+	test_output = RLE_compress(test_input)
+	if test_output != test_input {
+		t.Error("Failted rle compression on string: " + test_input + ". Output was: " + test_input)
+	}
 }
