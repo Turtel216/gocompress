b964a5d0889b6489e164f6d1850b1e3e53afc576
Turtel216 <papadimitrios216@gmail.com>
2024-06-19 23:52:13 +0300

    Added lzw test

--- internal/util/util.go
+++ internal/util/util.go
@@ -1,12 +1,1 @@
 package util
-
-// Utility function to get the last value of a map
-func GetLastValueOfMap(_map *map[string]int) int {
-	var last_value int
-
-	// Iterate over the map and get last value
-	for _, last_value = range *_map {
-	}
-
-	return last_value
-}
--- lzw_test.go
+++ lzw_test.go
@@ -7,12 +7,17 @@ import (
 
 func TestLZW_encoding(t *testing.T) {
 	test_input := "geekific-geekific"
-	// test_expected_output := "3abc2da3f"
+	var test_expected_output []int
+	test_expected_output = []int{103, 101, 101, 107, 105, 102, 105, 99, 45, 256, 258, 260, 262}
+
 	test_output := LZW_encoding(test_input)
 
-	fmt.Println(test_output)
+	for i, value := range test_output {
+		if test_expected_output[i] != test_output[i] {
+			t.Error("Failted rle compression on: " + fmt.Sprint(test_input[i]) +
+				". Output was: " + fmt.Sprint(value) +
+				". Expected output was: " + fmt.Sprint(test_expected_output[i]))
+		}
+	}
 
-	// if test_output != test_expected_output {
-	// 	t.Error("Failted rle compression on string: " + test_input + ". Output was: " + test_output)
-	// }
 }
